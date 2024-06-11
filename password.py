373114e83e8bb72c04ba08626f40b55d24f9a1fa
Yasas_Isuru <96908186+Yasasisuru@users.noreply.github.com>
2024-06-12 00:14:06 +0530

    update print segment

--- password.py
+++ password.py
@@ -2,4 +2,4 @@ import random
 password=int(input("enter the length of password -"))
 letter="abcdefghilklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%&*"
 randomPw="".join(random.sample(letter,password))
-print(randomPw)
+print("password - " + randomPw)
